7f2a81756b41b3ac9af844abeb65f8196f2c80c9
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2024-11-16 10:55:57 +0100

    Cwiczenia z kursu broCode

---
+++ przeciwprostokatna/obliczanie_przeciwprostakatnej/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class obliczanie_przeciwprostakatnej{
+    public static void Main(String[] args){
+        // Pobrac wartosc double a i b
+        // obliczyc pierwiastek kwadratu tych wartosci
+        // wyswietlic wynik
+
+        Console.WriteLine("Podaj pierwszą liczbę");
+
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę");
+
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double c = Math.Round(Math.Sqrt(a*a + b*b),2);
+
+        Console.WriteLine($"Przeciwprostokątna trojkąta o bokach a={a}, b={b} wynosi {c}");
+        
+
+    }
+}
---
+++ rysowanie_prostokata/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class rysowanieProstokata{
+public static void Main(string[] args){
+    int rows = 0;
+    int cols = 0;
+    string symbol = "";
+
+    while(symbol == "" || rows <= 0 || cols <=0){
+    System.Console.WriteLine("The number of rows:");
+    rows = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("The number of columns:");
+    cols = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("The symbol:");
+    symbol = Console.ReadLine();
+    }
+    Console.WriteLine("Projecting The Shape");
+        for (int i = 1; i <= rows; i++){
+        for(int j = 1; j <= cols; j++){
+            System.Console.Write(symbol);
+        }
+        Console.WriteLine("");
+    }
+}
+}
+
+
+
