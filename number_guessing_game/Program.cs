9779d762c157b928e5cdc41d5f55d66f12206459
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2024-11-17 11:46:25 +0100

    Zadanie - liczby_z_wielokropkiem

---
+++ number_guessing_game/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class numberGuessingGame{
+    public static void Main(string[] args){
+        int number;
+        int guess;
+        int tries;
+        int min = 1;
+        int max = 100;
+        String response = "";
+        bool continueGame = true;
+
+        while(continueGame){
+            guess = 0;
+            tries = 0;
+            number = new Random().Next(min,max+1);
+            Console.WriteLine($"Guess the number between {min} and {max}");
+            while(guess != number){
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    if(guess > number){
+                        Console.WriteLine("Too high");
+                    }else if(guess < number){
+                        Console.WriteLine("Too low");
+                    }
+                    tries++;
+                    
+                }catch(FormatException e){
+                    Console.WriteLine("Please enter a number");
+                    continue;
+                }catch(OverflowException e){
+                    Console.WriteLine("Number is too large or too small");
+                    continue;
+                }
+            }
+            Console.WriteLine($"The number was {number}");
+            Console.WriteLine($"You got it in {tries} tries");
+            Console.WriteLine("Would you like to play again? (y/n)");
+            response = Console.ReadLine();
+            response = response.ToLower();
+            if (response == "y")
+            {
+                continueGame = true;
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing!");
+                continueGame = false;
+            }
+           
+        }
+    }
+}
---
+++ zadanie_liczby_z_wielokropkiem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic; // Dodano dla List<T>
+using System.Linq; // Dodano dla Take() i TakeLast()
+
+namespace Zadania_petle
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Wczytywanie i parsowanie danych wejściowych
+            string wejscie = Console.ReadLine();
+            short[] dane = Array.ConvertAll<string, short>(wejscie.Split(" "), short.Parse);
+            short min = dane[0];
+            short max = dane[1];
+            short divider = dane[2];
+            if(dane[0] > dane[1])
+            {
+                min = dane[1];
+                max = dane[0];
+            }
+
+            List<short> allNumbers = new List<short>();
+            for(short i = (short)(min+1); i < max; i++)
+            {
+                allNumbers.Add(i);
+            }
+
+            allNumbers.RemoveAll(x => x % divider != 0);
+            if(allNumbers.Count <= 0){
+                Console.WriteLine("empty");
+                return;
+            }
+            string result;
+            if(allNumbers.Count > 10)
+            {
+                var firstThreeValues = allNumbers.Take(3).ToArray();
+                var twoLastValues = allNumbers.TakeLast(2).ToArray();
+                result = string.Join(", ", firstThreeValues) + ", ..., " + string.Join(", ", twoLastValues);
+            }
+            else
+            {
+                result = string.Join(", ", allNumbers);
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
