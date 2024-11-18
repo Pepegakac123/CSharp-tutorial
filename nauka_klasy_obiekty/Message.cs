8f30a041bc6375ffe4ac2dcd40a37b8727419b07
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2024-11-18 12:20:53 +0100

    Rysowanie wzorow - zadania

---
+++ nauka_klasy_obiekty/Message.cs
@@ -0,0 +1,9 @@
+namespace nauka_klasy_obiekty
+{
+    public static class Message
+    {
+        public static void Greet(string name){
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
---
+++ nauka_klasy_obiekty/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace nauka_klasy_obiekty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human firstHuman = new Human("Tomek", 16);
+            Human secondHuman = new Human("Andrzej", 32);
+
+            Message.Greet(firstHuman.name);
+            firstHuman.Eat("pasta");
+            firstHuman.Sleep();
+            Message.Greet(secondHuman.name);
+            secondHuman.Eat("eggs");
+            secondHuman.Sleep();
+        }
+    }
+
+    class Human
+    {
+    public string name;
+    public int age;
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+        public void Eat(string food){
+            Console.WriteLine($"{name} is eating {food}");
+        }
+
+        public void Sleep(){
+            Console.WriteLine($"{name} is sleeping");
+        }
+    }
+}
