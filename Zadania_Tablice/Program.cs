df2efdff94c14f431002ab82c7b93973e6ca3894
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2024-11-26 10:39:37 +0100

    Zadanie z tablicami

---
+++ Zadania_Tablice/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Zadania_Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] {0, 1, 1, 2, 3, 3, 3};
+            var b = new int[] {0, 1, 2, 3, 3};
+            Print(a, b);
+        }
+
+        // public static void Print(int[] a, int[] b)
+        // {
+        //     int[] temp = new int[a.Length];
+        //     int count = 0;
+
+        //     for (int i = 0; i < a.Length; i++)
+        //     {
+        //         bool exists = false;
+
+        //         for (int j = 0; j < b.Length; j++)
+        //         {
+        //             if (a[i] == b[j])
+        //             {
+        //                 exists = true;
+        //                 break;
+        //             }
+        //         }
+
+        //         if (!exists)
+        //         {
+        //             bool alreadyAdded = false;
+        //             for (int k = 0; k < count; k++)
+        //             {
+        //                 if (temp[k] == a[i])
+        //                 {
+        //                     alreadyAdded = true;
+        //                     break;
+        //                 }
+        //             }
+
+        //             if (!alreadyAdded)
+        //             {
+        //                 temp[count] = a[i];
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        //     if (count == 0)
+        //     {
+        //         Console.WriteLine("empty");
+        //         return;
+        //     }
+
+        //     for (int i = 0; i < count - 1; i++)
+        //     {
+        //         for (int j = 0; j < count - i - 1; j++)
+        //         {
+        //             if (temp[j] > temp[j + 1])
+        //             {
+        //                 int tmp = temp[j];
+        //                 temp[j] = temp[j + 1];
+        //                 temp[j + 1] = tmp;
+        //             }
+        //         }
+        //     }
+        //     for (int i = 0; i < count; i++)
+        //     {
+        //         Console.Write(temp[i] + " ");
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        // Part 3
+
+        public static void Print(int[] a, int[] b)
+        {
+            int[] result = new int[b.Length + a.Length];
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!Array.Exists(b, x => x == a[i]))
+                {
+                    // Sprawdzanie, czy element już został dodany do wyniku
+                    bool alreadyAdded = false;
+                    for (int j = 0; j < count; j++)
+                    {
+                        if (result[j] == a[i])
+                        {
+                            alreadyAdded = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyAdded)
+                    {
+                        result[count] = a[i];
+                        count++;
+                    }
+                }
+            }
+
+             for (int i = 0; i < b.Length; i++)
+            {
+                if (!Array.Exists(a, x => x == b[i]))
+                {
+                    // Sprawdzanie, czy element już został dodany do wyniku
+                    bool alreadyAdded = false;
+                    for (int j = 0; j < count; j++)
+                    {
+                        if (result[j] == b[i])
+                        {
+                            alreadyAdded = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyAdded)
+                    {
+                        result[count] = b[i];
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            Array.Resize(ref result, count);
+
+            Array.Sort(result);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
--- objetosc_stozka/Program.cs
+++ objetosc_stozka/Program.cs
@@ -1,45 +1,163 @@
-﻿using System;
+﻿// using System;
+
+// namespace objetosc_stozka
+// {
+//     class Program{
+       
+//         static void Main(string[] args){
+//              try{
+//                    string[] input = Console.ReadLine().Split(" ");
+//         double R = double.Parse(input[0]);
+//         double L = double.Parse(input[1]);
+//         if(L<R){throw new ArgumentException("obiekt nie istnieje");}
+//         if(R < 0 || L < 0){throw new ArgumentException("ujemny argument");}
+//         double V;
+//         int[] szacowaneWartosci = SzacowanieStozka(ObjetoscStozka(R, L, out V));
+//         if(szacowaneWartosci[0] < 0 || szacowaneWartosci[1] < 0){throw new OverflowException("ujemny argument");}
+//         Console.WriteLine($"{szacowaneWartosci[0]} {szacowaneWartosci[1]}");
+//             }catch(ArgumentException e){
+//                 Console.WriteLine(e.Message);
+//             }catch(OverflowException e){
+//                 Console.WriteLine(e.Message);
+//             }
+        
+//         }
+
+
+//         static double ObjetoscStozka(double R, double L, out double V){
+//             double H = Math.Sqrt(Math.Pow(L, 2) - Math.Pow(R, 2));
+//             V = (1.0 / 3.0) * Math.PI * Math.Pow(R, 2) * H;
+//             return V;
+//         }
+
+//         static int[] SzacowanieStozka(double V){
+//             int lowerLimit = (int)Math.Floor(V);
+//             int upperLimit = (int)Math.Ceiling(V);
+//             if (V < 1e-6) // Bardzo mała objętość
+//             {
+//                 lowerLimit = 0;
+//                 upperLimit = 0;
+//             }
+//             return new int[] {lowerLimit, upperLimit};
+
+//         } 
+//     }
+// }
+
+using System;
 
 namespace objetosc_stozka
 {
-    class Program{
-       
-        static void Main(string[] args){
-             try{
-                   string[] input = Console.ReadLine().Split(" ");
-        double R = double.Parse(input[0]);
-        double L = double.Parse(input[1]);
-        if(L<R){throw new ArgumentException("obiekt nie istnieje");}
-        if(R < 0 || L < 0){throw new ArgumentException("ujemny argument");}
-        double V;
-        int[] szacowaneWartosci = SzacowanieStozka(ObjetoscStozka(R, L, out V));
-        if(szacowaneWartosci[0] < 0 || szacowaneWartosci[1] < 0){throw new OverflowException("ujemny argument");}
-        Console.WriteLine($"{szacowaneWartosci[0]} {szacowaneWartosci[1]}");
-            }catch(ArgumentException e){
-                Console.WriteLine(e.Message);
-            }catch(OverflowException e){
-                Console.WriteLine(e.Message);
-            }
-        
-        }
+    class Program
+    {
+        // Stała epsilon do porównań zmiennoprzecinkowych
+        private const double EPSILON = 1e-10;
 
-
-        static double ObjetoscStozka(double R, double L, out double V){
-            double H = Math.Sqrt(Math.Pow(L, 2) - Math.Pow(R, 2));
-            V = (1.0 / 3.0) * Math.PI * Math.Pow(R, 2) * H;
-            return V;
-        }
-
-        static int[] SzacowanieStozka(double V){
-            int lowerLimit = (int)Math.Floor(V);
-            int upperLimit = (int)Math.Ceiling(V);
-            if (V < 1e-6) // Bardzo mała objętość
+        static void Main(string[] args)
+        {
+            try
             {
-                lowerLimit = 0;
-                upperLimit = 0;
-            }
-            return new int[] {lowerLimit, upperLimit};
+                // Rozszerzona obsługa wejścia z dodatkową walidacją
+                string[] input = Console.ReadLine().Split(" ");
+                
+                // Sprawdzenie poprawności liczby argumentów
+                if (input.Length != 2)
+                {
+                    throw new ArgumentException("ujemny argument");
+                }
 
-        } 
+                decimal R = decimal.Parse(input[0]);
+                decimal L = decimal.Parse(input[1]);
+
+                // Rozszerzone walidacje
+                ValidateInput(R, L);
+
+                decimal V;
+                decimal volume = (decimal)ObliczObjetoscStozka(R, L, out V);
+
+                // Zaawansowane szacowanie wartości
+                int[] szacowaneWartosci = SzacowanieStozka(volume);
+
+                Console.WriteLine($"{szacowaneWartosci[0]} {szacowaneWartosci[1]}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ujemny argument");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("obiekt nie istnieje");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Walidacja danych wejściowych
+        static void ValidateInput(decimal R, decimal L)
+        {
+            // Sprawdzenie wartości ujemnych
+            if (R < 0 || L < 0)
+            {
+                throw new ArgumentException("ujemny argument");
+            }
+
+            // Sprawdzenie warunku istnienia stożka
+            if (L < R)
+            {
+                throw new ArgumentException("obiekt nie istnieje");
+            }
+
+            // Zabezpieczenie przed bardzo małymi wartościami
+            if (R < (decimal)EPSILON || L < (decimal)EPSILON)
+            {
+                throw new ArgumentException("obiekt nie istnieje");
+            }
+        }
+
+        // Ulepszona metoda obliczania objętości stożka
+        static double ObliczObjetoscStozka(decimal R, decimal L, out decimal V)
+        {
+            // Obliczenie wysokości z zabezpieczeniami
+            decimal H = (decimal)Math.Sqrt((double)(L * L - R * R));
+
+            // Zabezpieczenie przed błędami numerycznymi
+            if (H<0 || (H == 0))
+            {
+                throw new ArithmeticException("ujemny argument");
+            }
+
+            // Dokładne obliczenie objętości z użyciem decimal
+            V = (1.0m / 3.0m) * (decimal)Math.PI * R * R * H;
+
+            return (double)V;
+        }
+
+        // Ulepszone szacowanie wartości
+        static int[] SzacowanieStozka(decimal V)
+        {
+            // Dodatkowe sprawdzenia
+            if (V < (decimal)EPSILON)
+            {
+                return new int[] { 0, 0 };
+            }
+
+            // Dokładniejsze zaokrąglanie z małą tolerancją
+            int lowerLimit = (int)Math.Floor((double)V + EPSILON);
+            int upperLimit = (int)Math.Ceiling((double)V - EPSILON);
+
+            // Dodatkowe zabezpieczenie przed przepełnieniem
+            if (lowerLimit < 0 || upperLimit < 0)
+            {
+                throw new OverflowException("ujemny argument");
+            }
+
+            return new int[] { lowerLimit, upperLimit };
+        }
     }
 }
