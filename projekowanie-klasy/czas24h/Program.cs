0a5f5850b56e1c9c226356f603b7eebfc66dd83e
Kacper Adamczyk <kacper.adamczyk@microsoft.wsei.edu.pl>
2024-12-16 14:30:41 +0100

    czas24h properties - wykonany

---
+++ projekowanie-klasy/czas24h/Program.cs
@@ -0,0 +1,69 @@
+﻿public class Czas24h
+{
+    private int liczbaSekund;
+
+    public int Sekunda
+    {
+        get => liczbaSekund - Godzina * 60 * 60 - Minuta * 60;
+        set
+        {
+            if (value < 0 || value > 59)
+                throw new ArgumentException("error");
+            liczbaSekund = Godzina * 3600 + Minuta * 60 + value;
+        }
+    }
+
+    public int Minuta
+    {
+        get => (liczbaSekund / 60) % 60;
+        set
+        {
+            if (value < 0 || value > 59)
+                throw new ArgumentException("error");
+            liczbaSekund = Godzina * 3600 + value * 60 + Sekunda;
+        }
+    }
+
+    public int Godzina
+    {
+        get => liczbaSekund / 3600;
+        set
+        {
+            if (value < 0 || value > 23)
+                throw new ArgumentException("error");
+            liczbaSekund = value * 3600 + Minuta * 60 + Sekunda;
+        }
+    }
+
+    public Czas24h(int godzina, int minuta, int sekunda)
+    {
+        if (sekunda < 0 || sekunda > 59 || minuta < 0 || minuta > 59 || godzina < 0 || godzina > 23)
+            throw new ArgumentException("error");
+        // uzupełnij kod zgłaszając wyjątek ArgumentException
+        // w sytuacji niepoprawnych danych
+
+        liczbaSekund = sekunda + 60 * minuta + 3600 * godzina;
+    }
+
+    public override string ToString() => $"{Godzina}:{Minuta:D2}:{Sekunda:D2}";
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Test 5
+        try
+        {
+            var t = new Czas24h(24, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 23;
+            t.Godzina = 1;
+            Console.WriteLine(t);
+
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ projekowanie-klasy/klasaPerson-wlasciwosci/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace klasaPerson_wlasciwosci
+{
+    using System.Linq;
+    public class Person
+    {
+        private string familyName;
+        private string firstName;
+        private DateTime birthday;
+
+        public Person(string familyName, string firstName, DateTime birthday)
+        {
+            FamilyName = familyName;
+            FirstName = firstName;
+            Birthday = birthday;
+        }
+
+        public string FamilyName
+        {
+            get => familyName;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                string processedValue = value.Trim();
+                if (!processedValue.All(c => char.IsLetter(c) || c == '-'))
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                string[] parts = processedValue.Split('-');
+                if (parts.Length > 2)
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                foreach (string part in parts)
+                {
+                    if (part.Length < 2)
+                        throw new ArgumentException("Incorrect data for FamilyName");
+                    if (!char.IsUpper(part[0]) || !part.Skip(1).All(char.IsLower))
+                        throw new ArgumentException("Incorrect data for FamilyName");
+                }
+                familyName = processedValue;
+            }
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("Incorrect data for FamilyName");
+                string processedValue = value.Trim();
+                if (!processedValue.All(c => char.IsLetter(c)))
+                    throw new ArgumentException("Incorrect data for FirstName");
+                if (processedValue.Length < 2 || !char.IsUpper(processedValue[0]) || !processedValue.Skip(1).All(char.IsLower))
+                    throw new ArgumentException("Incorrect data for FirstName");
+                firstName = processedValue;
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get => birthday;
+            set
+            {
+                // Sprawdzamy czy data nie jest domyślna (01.01.0001)
+                if (value == default(DateTime))
+                    throw new ArgumentException("Incorrect data for Birthday");
+
+                // Sprawdzamy czy data nie jest z przyszłości
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Incorrect data for Birthday");
+
+                // Sprawdzamy czy rok jest sensowny (np. nie wcześniej niż 1900)
+                if (value.Year < 1900)
+                    throw new ArgumentException("Incorrect data for Birthday");
+                birthday = value;
+            }
+        }
+
+        public override string ToString() =>
+            $"{FirstName} {FamilyName} ({Birthday:yyyy-MM-dd})";
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Test: Utworzenie obiektu, dane poprawne */
+            var p1 = new Person(familyName: "Abacki",
+                                firstName: "John",
+                                birthday: new DateTime(year: 2000, month: 1, day: 1)
+                               );
+            Console.WriteLine(p1.FamilyName);
+            Console.WriteLine(p1.FirstName);
+            Console.WriteLine($"{p1.Birthday:yyyy-MM-dd}");
+        }
+    }
+}
