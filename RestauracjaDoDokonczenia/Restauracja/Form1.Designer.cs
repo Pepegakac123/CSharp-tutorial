f9683fa56cba2051835353fe4b16811420d23bb3
Kacper Adamczyk <113251322+Pepegakac123@users.noreply.github.com>
2025-05-18 12:23:19 +0200

    Projekt restauracji mietka

---
+++ RestauracjaDoDokonczenia/Restauracja/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Restauracja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTableNr = new System.Windows.Forms.NumericUpDown();
+            this.listBoxOrdered = new System.Windows.Forms.ListBox();
+            this.listBoxReady = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTableNr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(509, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(19, 57);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(299, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nazwa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 27);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "NrStolika";
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.DecimalPlaces = 2;
+            this.numericUpDownPrice.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Location = new System.Drawing.Point(337, 57);
+            this.numericUpDownPrice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownPrice.TabIndex = 5;
+            // 
+            // numericUpDownTableNr
+            // 
+            this.numericUpDownTableNr.Location = new System.Drawing.Point(449, 57);
+            this.numericUpDownTableNr.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDownTableNr.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDownTableNr.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownTableNr.Name = "numericUpDownTableNr";
+            this.numericUpDownTableNr.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownTableNr.TabIndex = 6;
+            this.numericUpDownTableNr.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // listBoxOrdered
+            // 
+            this.listBoxOrdered.FormattingEnabled = true;
+            this.listBoxOrdered.Location = new System.Drawing.Point(22, 141);
+            this.listBoxOrdered.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxOrdered.Name = "listBoxOrdered";
+            this.listBoxOrdered.Size = new System.Drawing.Size(511, 368);
+            this.listBoxOrdered.TabIndex = 7;
+            // 
+            // listBoxReady
+            // 
+            this.listBoxReady.FormattingEnabled = true;
+            this.listBoxReady.Location = new System.Drawing.Point(632, 71);
+            this.listBoxReady.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxReady.Name = "listBoxReady";
+            this.listBoxReady.Size = new System.Drawing.Size(539, 433);
+            this.listBoxReady.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(632, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Produkty gotowe do wydania";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(24, 591);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 588);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 21);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Oplacenie rachunku";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(287, 591);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(92, 20);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1228, 643);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxReady);
+            this.Controls.Add(this.listBoxOrdered);
+            this.Controls.Add(this.numericUpDownTableNr);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTableNr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDownTableNr;
+        private System.Windows.Forms.ListBox listBoxOrdered;
+        private System.Windows.Forms.ListBox listBoxReady;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RestauracjaDoDokonczenia/Restauracja/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restauracja
+{
+    public partial class Form1 : Form
+    {
+        string pathToOrderedFile = @"D:\Users\kacper.adamczyk.WSEI\CSharp-tutorial\RestauracjaDoDokonczenia\ordered.xml";
+        List<OrderedProducts> orderedProducts = new List<OrderedProducts>();
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                orderedProducts = OrderedProductsXmlSerializer.DeserializeListFromXml(pathToOrderedFile);
+                RefreshProductList();
+            }
+            catch { }
+        }
+        
+        void RefreshProductList()
+        {
+            listBoxOrdered.Items.Clear();
+            foreach (OrderedProducts product in orderedProducts)
+            {
+                string strToAdd = product.tableNr.ToString() + ".   " + product.productName + ":    " + product.price.ToString() + "zł";
+                listBoxOrdered.Items.Add(strToAdd);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OrderedProducts productToAdd;
+            productToAdd.price = numericUpDownPrice.Value;
+            productToAdd.tableNr = (uint)numericUpDownTableNr.Value;
+            productToAdd.productName = textBoxName.Text;
+            orderedProducts.Add(productToAdd);
+
+            RefreshProductList();
+
+            OrderedProductsXmlSerializer.SerializeListToXml(orderedProducts,pathToOrderedFile);
+        }
+
+
+        List<OrderedProducts> readyProducts = new List<OrderedProducts>();
+
+        void RefreshReadyProductList()
+        {
+            listBoxReady.Items.Clear();
+            foreach (OrderedProducts product in readyProducts)
+            {
+                string strToAdd = product.tableNr.ToString() + ".   " + product.productName + ":    " + product.price.ToString() + "zł";
+                listBoxReady.Items.Add(strToAdd);
+            }
+        }
+
+        string pathToReadyFile = @"D:\Users\kacper.adamczyk.WSEI\CSharp-tutorial\RestauracjaDoDokonczenia\ready.xml";
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                readyProducts = OrderedProductsXmlSerializer.DeserializeListFromXml(pathToReadyFile);
+                RefreshReadyProductList();
+            }
+            catch { }
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            uint selectedTable = (uint)numericUpDown1.Value;
+            decimal totalForTable = CalculateTotalForTable(selectedTable);
+
+            MessageBox.Show(
+                $"Suma rachunku dla stolika {selectedTable}: {totalForTable}zł",
+                "Rachunek",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uint selectedTable = (uint)numericUpDown1.Value;
+            decimal totalForTable = CalculateTotalForTable(selectedTable);
+            decimal paymentAmount = numericUpDown2.Value;
+
+            if (paymentAmount < totalForTable)
+            {
+                MessageBox.Show(
+                    $"Wprowadzona kwota ({paymentAmount}zł) jest mniejsza niż suma rachunku ({totalForTable}zł).\n" +
+                    $"Brakuje: {totalForTable - paymentAmount}zł",
+                    "Niewystarczająca kwota",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return; 
+            }
+            DialogResult result = MessageBox.Show(
+                $"Czy na pewno opłacić rachunek dla stolika {selectedTable} w wysokości {totalForTable}zł?",
+                "Potwierdzenie płatności",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                orderedProducts.RemoveAll(p => p.tableNr == selectedTable);
+
+                readyProducts.RemoveAll(p => p.tableNr == selectedTable);
+                OrderedProductsXmlSerializer.SerializeListToXml(orderedProducts, pathToOrderedFile);
+                OrderedProductsXmlSerializer.SerializeListToXml(readyProducts, pathToReadyFile);
+
+
+                RefreshProductList();
+                RefreshReadyProductList();
+
+                MessageBox.Show(
+                    $"Rachunek dla stolika {selectedTable} w wysokości {totalForTable}zł został opłacony.",
+                    "Rachunek opłacony",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            uint selectedTable = (uint)numericUpDown1.Value;
+            decimal totalForTable = CalculateTotalForTable(selectedTable);
+            decimal paymentAmount = numericUpDown2.Value;
+        }
+
+        private decimal CalculateTotalForTable(uint tableNumber)
+        {
+            decimal total = 0;
+
+            foreach (OrderedProducts product in orderedProducts)
+            {
+                if (product.tableNr == tableNumber)
+                {
+                    total += product.price;
+                }
+            }
+
+            foreach (OrderedProducts product in readyProducts)
+            {
+                if (product.tableNr == tableNumber)
+                {
+                    total += product.price;
+                }
+            }
+
+            return total;
+        }
+
+    }
+}
